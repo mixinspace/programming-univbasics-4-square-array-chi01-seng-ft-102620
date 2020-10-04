9482932ea10b19aeb2ee7d0c3a1d337fe52c5a01
Islam Madkaur <islammadkaur@gmail.com>
2020-10-04 20:50:40 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   count = 0
   new_numbers = []
-  while count < array.length {
+  while count < array.length do
     new_numbers.push(array[count]**2)
     count += 1
-  }
+  end
   new_numbers
 end
